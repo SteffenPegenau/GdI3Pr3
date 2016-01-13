cf256d189b3e8bc6f61dfdb31c71031dd6784fd5
Steffen Pegenau <steffen.pegenau@gmail.com>
2016-01-13 19:11:06 +0100

    Bilateralfilter scheint zu funktionieren

--- src/algorithms/Cartoonize.cpp
+++ src/algorithms/Cartoonize.cpp
@@ -4,16 +4,51 @@
 #include <cmath>
 
 int Cartoonize::toValidCoord(int &i, const int &max) {
-    if(i < 0) return 0;
-    if(i >= max) return max;
+    if (i < 0) return 0;
+    if (i >= max) return max - 1;
     return i;
+}
+
+/**
+ * Berechnet die raeumliche Charakteristik zweier Koordinaten
+ */
+double Cartoonize::c_domain(int sigmaD, int x1, int y1, int x2, int y2) {
+    double result = pow((x1 - x2), 2) + pow((y1 - y2), 2); // Zähler
+    result = result / (2 * pow(sigmaD, 2)); // Nenner
+    result *= -1;
+    return result; // Vorzeichen
+}
+
+/**
+ * Berechnet die farbliche Distanz zweier Pixel
+ */
+double Cartoonize::colorDistance(const Pixel p1, const Pixel p2) {
+    return sqrt(
+            pow(p1.r - p2.r, 2) +
+            pow(p1.g - p2.g, 2) +
+            pow(p1.b - p2.b, 2)
+            );
+}
+
+/**
+ * Berechnet die farbliche Charakteristik zweier Pixel
+ */
+double Cartoonize::c_edge(const Image &f, const int sigmaR, int x1, int y1, int x2, int y2) {
+    const Pixel p1 = f[y1][x1];
+    const Pixel p2 = f[y2][x2];
+    double cd = colorDistance(p1, p2);
+    return  -pow(cd, 2) / (2 * pow(sigmaR, 2));
 }
 
 /**
  * Berechnet das Filtergewicht eines Pixelpaares 
  */
-double Cartoonize::filterWeight(const Image &f, int &x1, int &y1, int &x2, int &y2) {
-    return 1.0;
+double Cartoonize::filterWeight(const Image &f, const int sigmaD, const int sigmaR, int x1, int y1, int x2, int y2) {
+    //return exp(c_domain(sigmaD, x1, y1, x2, y2));
+    double cdomain = c_domain(sigmaD, x1, y1, x2, y2);
+    double cedge = c_edge(f, sigmaR, x1, y1, x2, y2);
+    //double cedge = 0;
+    return exp(cdomain + cedge);
 }
 
 /**
@@ -27,37 +62,37 @@ Image Cartoonize::bilateralFilter(const Image& src, const int& filterSize, const
     int range = (filterSize - 1) / 2;
     double w = 0;
     double wSum = 0;
-    Pixel sum;
+    double red, green, blue;
     int tmpX, tmpY;
-    
+
     for (int y = 0; y < Y; y++) {
         for (int x = 0; x < X; x++) {
             // Init für neue Pixelberechnung
-            sum.r = 0;
-            sum.g = 0;
-            sum.b = 0;
+            Pixel &p = g[y][x];
+            red = 0;
+            green = 0;
+            blue = 0;
             wSum = 0;
             for (int ry = y - range; ry <= y + range; ry++) {
-                for (int rx = x - range; rx <= x+range; rx++) {
-                    w = filterWeight(src, x, y, rx, ry);
-                    //const Pixel &f = getPixel(src, X, Y,rx, ry);
+                for (int rx = x - range; rx <= x + range; rx++) {
+                    w = filterWeight(src, sigmaD, sigmaR, toValidCoord(x, X), toValidCoord(y, Y), toValidCoord(rx, X), toValidCoord(ry, Y));
                     tmpX = toValidCoord(rx, X);
                     tmpY = toValidCoord(ry, Y);
                     const Pixel &f = src[tmpY][tmpX];
-                    
+
                     // Zähler
-                    sum.r += f.r * w;
-                    sum.g += f.g * w;
-                    sum.b += f.b * w;
-                    
+                    red += f.r * w;
+                    green += f.g * w;
+                    blue += f.b * w;
+
                     // Nenner
                     wSum += w;
+
                 }
             }
-            Pixel &p = g[y][x];
-            g[y][x].r = sum.r / wSum;
-            g[y][x].g = sum.g / wSum;
-            g[y][x].b = sum.b / wSum;
+            p.r = red / wSum;
+            p.g = green / wSum;
+            p.b = blue / wSum;
         }
     }
     return g;
@@ -91,50 +126,5 @@ void Cartoonize::process(const Parameters &params, const Image &src, Image &dst)
     Image g = bilateralFilter(src, filterSize, sigmaD, sigmaR);
 
     dst = g;
-}
-
-
-/**
- * Gibt korrekten Pixel zurück, 
- * auch wenn x oder y außerhalb des Bildes liegen
-
-const Pixel &Cartoonize::getPixel(const Image &pic, const int &width, const int &height, int x, int y) {
-    x = (x < 0) ? 0 : x;
-    x = (x >= width) ? width : x;
-    y = (y < 0) ? 0 : y;
-    y = (y >= height) ? height : y;
-    Pixel &f = pic[y][x];
-    return f;
-}
-*/
-
-
-
-/**
- * Berechnet die räumliche Distanz zweier Koordinatenkomponenten
- */
-double regionalDistanceSquare(int &x1, int &x2) {
-    
-}
-
-/**
- * Berechnet die farbliche Distanz zweier Pixel
- */
-double Cartoonize::colorDistanceSquare(Pixel &p1, Pixel & p2) {
-    
-}
-
-/**
- * Berechnet die raeumliche Charakteristik zweier Koordinaten
- */
-double Cartoonize::c_domain(int &x1, int &y1, int &x2, int &y2) {
-
-}
-
-/**
- * Berechnet die farbliche Charakteristik zweier Pixel
- */
-double Cartoonize::c_edge(const Image &f, int &x1, int &y1, int &x2, int &y2) {
-
 }
 
--- src/algorithms/Cartoonize.h
+++ src/algorithms/Cartoonize.h
@@ -9,17 +9,15 @@ public:
 
     Image bilateralFilter(const Image& src, const int& filterSize, const int& sigmaD, const int& sigmaR);
 
-    double c_domain(int &x1, int &y1, int &x2, int &y2);
-    double c_edge(const Image &f, int &x1, int &y1, int &x2, int &y2);
-
-    double regionalDistanceSquare(int &x1, int &x2);
-    double colorDistanceSquare(Pixel &p1, Pixel &p2);
+    double c_domain(int sigmaD, int x1, int y1, int x2, int y2);
+    double c_edge(const Image &f, const int sigmaR, const int x1, const int y1, const int x2, const int y2);
+    double colorDistance(const Pixel p1, const Pixel p2);
 
     const Pixel &getPixel(const Image &pic, const int &width, const int &height, int x, int y);
-    
+
     int toValidCoord(int &i, const int &max);
 
-    double filterWeight(const Image &f, int &x1, int &y1, int &x2, int &y2);
+    double filterWeight(const Image &f, const int sigmaD, const int sigmaR, int x1, int y1, int x2, int y2);
 };
 
 #endif
